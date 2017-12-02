3ef0f1830164d5b55edf14ac0f12367006489455
jkolden <john.kolden@gmail.com>
2017-12-02 12:00:38 -0800

    Update entertainment_center.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,46 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A Story of a boy and his toys that come to life",
-                        "https://tinyurl.com/y8jrdmex",
-                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-avatar = media.Movie("Avatar",
-                     "A marine on an alien planet",
-                     "https://tinyurl.com/ydcvt8es",
-                     "https://www.youtube.com/watch?v=5PSNL1qE6VY")
+# Creating movies to describe the instances of the Class Movie
 
-school_of_rock = media.Movie("School of Rock",
-                             "A teacher forms a band with his students",
-                             "https://tinyurl.com/yclzd8xv",
-                             "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
-searching_for_sugarman = media.Movie("Searching For Sugarman",
-                                     "Academy Award winning"
-                                     "documentary about a 1960's folk icon",
-                                     "https://tinyurl.com/yc5xfhwm",
-                                     "https://tinyurl.com/al4r7gu")
+toy_story = media.Movie(
+    "Toy Story",
+    "A Story of a boy and his toys that come to life",
+    "https://tinyurl.com/y8jrdmex",
+    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-all_the_presidents_men = media.Movie("All the President's Men",
-                                     "Two reporters from the Washington Post"
-                                     "uncover illegal activity at the highest"
-                                     "levels of government",
-                                     "https://tinyurl.com/ydcj9pdc",
-                                     "https://tinyurl.com/lgl4e79")
+avatar = media.Movie(
+    "Avatar",
+    "A marine on an alien planet",
+    "https://tinyurl.com/ydcvt8es",
+    "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-moneyball = media.Movie("Moneyball",
-                        "An unconventional general manager "
-                        "transforms the Oakland A's",
-                        "https://tinyurl.com/y7xoptwz",
-                        "https://www.youtube.com/watch?v=AiAHlZVgXjk")
+school_of_rock = media.Movie(
+    "School of Rock",
+    "A teacher forms a band with his students",
+    "https://tinyurl.com/yclzd8xv",
+    "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
+
+searching_for_sugarman = media.Movie(
+    "Searching For Sugarman",
+    "Academy Award winning documentary about a 1960's folk icon",
+    "https://tinyurl.com/yc5xfhwm",
+    "https://tinyurl.com/al4r7gu")
+
+all_the_presidents_men = media.Movie(
+    "All the President's Men",
+    "Two reporters from the Washington Post uncover "
+    "illegal activity at the highest levels of government",
+    "https://tinyurl.com/ydcj9pdc",
+    "https://tinyurl.com/lgl4e79")
+
+moneyball = media.Movie(
+    "Moneyball",
+    "An unconventional general manager transforms the Oakland A's",
+    "https://tinyurl.com/y7xoptwz",
+    "https://www.youtube.com/watch?v=AiAHlZVgXjk")
 
 movies = [toy_story,
           avatar,
